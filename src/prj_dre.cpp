91210c458c5e2f0e208c7718ae8ef1639ff96bac
Txinto Vaz <txinto@gatatac.org>
2017-12-09 17:14:43 +0000

    Clean code from past projects

--- src/prj_dre.cpp
+++ src/prj_dre.cpp
@@ -7,54 +7,6 @@ t_dre dre;
 void dre_init(void){
   uint8_t i;
 
-  //define el valor RGB de la tira de comida, se le asigna un valor en el setup (rojo oscuro)
-  dre.veneno.r  = 228;
-  dre.veneno.g  = 155;
-  dre.veneno.b  = 192;
-
-  //define el valor RGB de la tira de veneno, se le asigna un valor en el setup (azul claro)
-  dre.comida.r  = 192;
-  dre.comida.g  = 232;
-  dre.comida.b  = 229;
-
-  dre.cont_ciclos_prueba = 0; //lleva la cuenta del numero de ciclos realizados en la prueba(sensores-actuadores)
-
-  //Variables para almacenar el valor RGB del sensor de forma independiente
-  dre.suelo.r = 0;
-  dre.suelo.g = 0;
-  dre.suelo.b = 0;
-
-  //Variable que contienen el valor de la velocidad del motor en el rango
-  dre.m_izq = 0;
-  dre.m_der = 0;
-
-////////=======================================================Variables temporales==========================================================================///////////////////////////////////////
-
-  dre.state = 0; //sirve para la calibracion del sensor de luz
-  dre.coincidencia = 0;
-  dre.confirmacion = 0;// sirve para confirmar el estado de la comunicacion
-  dre.temp = 0; //es usada en la comunicacion para guardar las instrucciones de navegacion
-  dre.temp1 = 0; //variable usada dentro de las operaciones de cambio de rango para las salidas
-  dre.n = 0; //variable para navegar por la tira de LED's
-  dre.habilita_func = false;//variable usada en la secuencia de inicio
-  dre.num_comms = 0;//variable temporal usada en la secuencia de inicio
-
-  // Inicializamos las neuronas de entrada
-  dre.i_neurona.comida = 0;
-  dre.i_neurona.veneno = 0;
-  dre.i_neurona.n2 = 0;
-  dre.i_neurona.n3 = 0;
-  dre.i_neurona.n4 = 0;
-  dre.i_neurona.n5 = 0;
-  dre.i_neurona.n6 = 0;
-  dre.i_neurona.n7 = 0;
-  dre.i_neurona.n8 = 0;
-
-  // Inicializamos las neuronas de salida
-  dre.o_neurona.izq = 0;
-  dre.o_neurona.der = 0;
-  dre.o_neurona.leds = 0;
-
   dre.iot_connected = false;
   dre.iot_go_online = false;
   dre.iot_go_offline = false;
@@ -62,8 +14,6 @@ void dre_init(void){
   dre.iot_disconnect = false;
   dre.iot_publish = false;
   dre.iot_publish_timer = 0;
-
-  dre.looptime = 0;
 
   dre.emgcy_timer = 0;
   dre.emgcy_button = false;
@@ -85,6 +35,5 @@ void dre_init(void){
     dre.cmd_storage[dre.cmd_counter][0];
   }
   dre.cmd_counter = 0;
-
 
 }
--- src/prj_dre.h
+++ src/prj_dre.h
@@ -7,64 +7,6 @@
 #include "prj_cfg.h"
 
 typedef struct {
-  int r;
-  int g;
-  int b;
-}t_color;
-
-typedef struct {
-  int comida;
-  int veneno;
-  int n2;
-  int n3;
-  int n4;
-  int n5;
-  int n6;
-  int n7;
-  int n8;
-}t_in_neurona;
-
-typedef struct {
-  int izq;
-  int der;
-  int leds;
-}t_out_neurona;
-
-
-typedef struct {
-  t_color veneno;
-  t_color comida;
-  t_color suelo;
-  t_color cuadrante[4];
-  int cont_ciclos_prueba;//lleva la cuenta del numero de ciclos realizados en la prueba(sensores-actuadores)
-
-  //Definimos el vector que contendra los valores de todas las neuronas
-  t_in_neurona i_neurona;
-  t_out_neurona o_neurona; //vector de las neuronas de salida
-
-  //Variable que contienen el valor de la velocidad del motor en el rango
-  signed int m_izq;
-  signed int m_der;
-
-////////=======================================================Variables temporales==========================================================================///////////////////////////////////////
-
-  int state; //sirve para la calibracion del sensor de luz
-  int coincidencia;
-  int confirmacion;// sirve para confirmar el estado de la comunicacion por la parte del arduino
-  int temp; //es usada en la comunicacion para guardar las instrucciones de navegacion
-  unsigned int temp1; //variable usada dentro de las operaciones de cambio de rango para las salidas
-  int n; //variable para navegar por la tira de LEDs
-
-
-  String serial_str;
-  int serial_int;
-
-  bool habilita_func;//variable usada en la secuencia de inicio
-  int num_comms;//variable temporal usada en la secuencia de inicio
-  int timer_comm; // Temporizador para las esperas
-
-  bool boton_inicio;  // Variable para gestionar la pulsacion del boton de inicio.
-  int timer_boton;    // Variable para gestionar el filtrado por tiempo del boton.
 
   /*** IoT Management ***/
   bool iot_connected;
